890efcf6efe72493234b565f7565d4abfee56360
sashag <a.f.gurevich@gmail.com>
2021-04-26 13:04:44 +0300

    name bugfix

--- .convert.sh
+++ .convert.sh
@@ -3,6 +3,6 @@ mkdir output
 
 for f in ./mds/*; do
     FILE_NAME=${f##*/}
-    NEW_FILE_NAME=${FILE_NAME/%.md/.pdf}
-    `pandoc ${f} -s -o output/${NEW_FILE_NAME}`
+    NEW_FILE_NAME=${FILE_NAME%%.md}.pdf
+    `pandoc ${f} -t pdf -s -o output/${NEW_FILE_NAME}`
 done
